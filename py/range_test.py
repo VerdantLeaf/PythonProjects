3e00de146f2a26fcc2422da0ddc4a7110a20fcdf
VerdantLeaf <111800371+VerdantLeaf@users.noreply.github.com>
2025-09-21 21:58:23 -0500

    Range test for RV stuff for school

--- py/range_test.py
+++ py/range_test.py
@@ -1,3 +1,9 @@
-addr_offsets = [i for i in range(-2048, 2048+1, 4)]
+addr_offsets = [i for i in range(-2048, 2047, 4)]
 
 print(addr_offsets)
+
+print("\n\n")
+
+reg_list = [f"x{i}" for i in range(2, 32)]
+
+print(reg_list)
